91c0bd290b1a9fd21059fa1d63abebad9cb254d1
ApcGamer <abhisingh3@hotmail.com>
2020-01-04 15:24:40 -0800

    Updates

---
+++ precache-manifest.0b903dd54d0e703b6757fde5c544c618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fabb512b7ded9d129c0df5f90c4a14",
+    "url": "/hello-monsters/index.html"
+  },
+  {
+    "revision": "c317bdcb3d43c4c18f28",
+    "url": "/hello-monsters/static/css/main.1ebdc4c4.chunk.css"
+  },
+  {
+    "revision": "9a9df13c800cf98ecfb6",
+    "url": "/hello-monsters/static/js/2.4dfeb44e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hello-monsters/static/js/2.4dfeb44e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c317bdcb3d43c4c18f28",
+    "url": "/hello-monsters/static/js/main.cd8ef8de.chunk.js"
+  },
+  {
+    "revision": "7ac6e070f8bdd3c9cd70",
+    "url": "/hello-monsters/static/js/runtime-main.7e055216.js"
+  }
+]);
